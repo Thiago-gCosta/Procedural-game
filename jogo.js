82ff7672c08b1ae9b11dd607476047f727b236bc
Thiago Gomes da Costa <145879617+Thiago-gCosta@users.noreply.github.com>
2023-12-21 19:34:51 -0300

    Add files via upload
    Atualização de inimigos e botão de respawn

--- jogo.js
+++ jogo.js
@@ -20,6 +20,9 @@ var X,
   Full,
   PickAxe,
   Mine = 0,
+  ER,
+  XE,
+  YE,
   pedras = 0;
 function Game() {
   for (Y = 0; Y != heigth; Y++) {
@@ -39,10 +42,11 @@ function Game() {
   }
   vetorProcedural[PickAxe][PickAxe] = "p";
   vetorProcedural[MovY][MovX] = "0";
+  vetorProcedural[width / Chek - 2][width / Chek - 2]="!";
   saida.innerHTML = vetorProcedural.join("\n", ",").replaceAll(",", "|");
 }
 function Procedural() {
-  X = 0;
+  X = 0; 
   Y = 0;
   for (
     B = 0;
@@ -69,6 +73,7 @@ function Procedural() {
       A == 0 || A == width / Chek - 1 ? (vetorProcedural[B][A] = "X") : (A = A);
     }
   }
+  Enemies();
 }
 
 function Cima() {
@@ -82,13 +87,28 @@ function Cima() {
       : (Mine = Mine);
     if (vetorProcedural[MovY - 1][MovX] == "X" && Mine > 0) {
       pedras++;
-      pedra.innerHTML = "X = " + pedras;
+      pedra.innerHTML = "Stones = " + pedras;
     }
     MovY--;
     vetorProcedural[MovY + 1][MovX] = " ";
     vetorProcedural[MovY][MovX] = "0";
     saida.innerHTML = vetorProcedural.join("\n", ",").replaceAll(",", "|");
+  }
+  if (MovX == width / Chek - 2 && MovY == width / Chek - 2) {
+    Redeclarar();
   } 
+  if (MovX > XE) {
+    XE++;
+  } else if (MovX < XE) {
+    XE--;
+  } else if (MovY > YE) {
+    YE++;
+  } else if (MovY < YE) {
+    YE--;
+  }else{
+    saida.innerHTML = "Fainted"
+  }
+  vetorProcedural[YE][XE] = "$"; 
 }
 
 function Esquerda() {
@@ -102,13 +122,28 @@ function Esquerda() {
       : (Mine = Mine);
     if (vetorProcedural[MovY][MovX - 1] == "X" && Mine > 0) {
       pedras++;
-      pedra.innerHTML = "X = " + pedras;
+      pedra.innerHTML = "Stones = " + pedras;
     }
     MovX--;
     vetorProcedural[MovY][MovX + 1] = " ";
     vetorProcedural[MovY][MovX] = "0";
     saida.innerHTML = vetorProcedural.join("\n", ",").replaceAll(",", "|");
   } 
+  if (MovX == width / Chek - 2 && MovY == width / Chek - 2) {
+    Redeclarar();
+  } 
+  if (MovX > XE) {
+    XE++;
+  } else if (MovX < XE) {
+    XE--;
+  } else if (MovY > YE) {
+    YE++;
+  } else if (MovY < YE) {
+    YE--;
+  } else {
+    saida.innerHTML = "Fainted";
+  }
+  vetorProcedural[YE][XE] = "$";
 }
 function Baixo() {
   if (
@@ -121,13 +156,28 @@ function Baixo() {
       : (Mine = Mine);
     if (vetorProcedural[MovY + 1][MovX] == "X" && Mine > 0) {
       pedras++;
-      pedra.innerHTML = "X = " + pedras;
+      pedra.innerHTML = "Stones = " + pedras;
     }
     MovY++;
     vetorProcedural[MovY - 1][MovX] = " ";
     vetorProcedural[MovY][MovX] = "0";
     saida.innerHTML = vetorProcedural.join("\n", ",").replaceAll(",", "|");
   } 
+  if (MovX == width / Chek - 2 && MovY == width / Chek - 2) {
+    Redeclarar();
+  } 
+  if (MovX > XE) {
+    XE++;
+  } else if (MovX < XE) {
+    XE--;
+  } else if (MovY > YE) {
+    YE++;
+  } else if (MovY < YE) {
+    YE--;
+  } else {
+    saida.innerHTML = "Fainted";
+  }
+  vetorProcedural[YE][XE] = "$";
 }
 function Direita() {
   if (
@@ -140,11 +190,55 @@ function Direita() {
       : (Mine = Mine);
     if (vetorProcedural[MovY][MovX + 1] == "X" && Mine > 0) {
       pedras++;
-      pedra.innerHTML = "X = " + pedras;
+      pedra.innerHTML = "Stones = " + pedras;
     }
     MovX++;
     vetorProcedural[MovY][MovX - 1] = " ";
     vetorProcedural[MovY][MovX] = "0";
     saida.innerHTML = vetorProcedural.join("\n", ",").replaceAll(",", "|");
   }
+  if (MovX == width / Chek - 2 && MovY == width / Chek - 2) {
+    Redeclarar();
+  } 
+  if (MovX > XE) {
+    XE++;
+  } else if (MovX < XE) {
+    XE--;
+  } else if (MovY > YE) {
+    YE++;
+  } else if (MovY < YE) {
+    YE--;
+  } else {
+    saida.innerHTML = "Fainted";
+  }
+  vetorProcedural[YE][XE] = "$"; 
+}
+function Redeclarar(){
+ vetorMapa = [];
+ vetorTex = ["X", " "];
+ vetorProcedural = [];
+ width = 100;
+ heigth = 100;
+   gerator = document.getElementById("mode");
+   MovX = 1;
+   MovY = 1;
+   Random;
+   Chek = 2;
+   ChekX = Chek;
+   ChekY = Chek;
+   OMGX = 0;
+   OMGY = 0;
+   Game();
+}
+function Enemies(){
+for (
+  ER = Math.ceil(Math.random() * (width / Chek));
+  vetorProcedural[ER][ER] != " ";
+
+) {
+  ER = Math.ceil((Math.random() * (width - 1)) / Chek);
+}
+YE=ER;
+XE=ER;
+vetorProcedural[YE][XE]="$";
 }
